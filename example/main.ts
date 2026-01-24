eb65866fec850adf47b9ee9b444a5002a0802bfc
Thomas Huber <thomas3577@gmx.net>
2026-01-24 20:24:17 +0100

    simplefied example

--- example/main.ts
+++ example/main.ts
@@ -1,13 +1,8 @@
 import { AlpineApp } from '../src/app.ts';
-import * as path from '@std/path';
-
-const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
 
 const app = new AlpineApp({
   app: {
     dev: true,
-    staticFilesPath: path.join(__dirname, 'public'),
-    staticExtensions: ['.css', '.js', '.png'],
   },
   oak: {
     listenOptions: {
